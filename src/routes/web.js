d52800a08542eb55297731a7f9971a20d1172625
SharmanineMangombe <sharmaine.mangombe@gmail.com>
2022-04-02 15:14:34 +0200

    my first node application

---
+++ src/routes/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+import homepageController from "../controlllers/homepageController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+router.get("/",homepageController.getHomepage);
+
+return app.use("/", router);
+
+};
+
+module.exports = initWebRoutes;
